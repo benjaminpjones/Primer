910c5634bacc87c5ce8d3eb2e2507fd27165341b
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-03-17 11:14:53 -0400

    Complete Exercizes 1.1 and 1.2.

---
+++ ch-1/ex-1-1.cpp
@@ -0,0 +1,10 @@
+// Exercise 1.1: Review the documentation for your compiler and determine what
+// file naming convention it uses. Compile and run the `main` program from
+// page 2.
+
+int main()
+{
+    return 0;
+}
+
+// The compiler I am using is g++, which creates a file called a.out by default.
---
+++ ch-1/ex-1-2.cpp
@@ -0,0 +1,11 @@
+// Exercise 1.2: Change the program to return -1. A return value of -1 is often 
+// treated as an indicator that the program failed. Recompile and rerun your
+// program to see how your system treats a failure indicator from main.
+
+int main()
+{
+    return -1;
+}
+
+// Using g++ on a Mac, there does not seem to be any indicator that the program
+// failed after running it in the Terminal.
