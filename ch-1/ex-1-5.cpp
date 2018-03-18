b445d523f7c20f4c29eb35e40d634b32df0911a4
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-03-18 18:57:28 -0400

    Complete Exercises 1.3, 1.4 and 1.5.

---
+++ ch-1/ex-1-5.cpp
@@ -0,0 +1,19 @@
+// Exercise 1.5: [In Exercise 1.4] we wrote the output in one large statement.
+// Rewrite the program to use a separate statement to print each command.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The product of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 * v2;
+    std::cout << ".";
+    std::cout << std::endl;
+    return 0;
+}
